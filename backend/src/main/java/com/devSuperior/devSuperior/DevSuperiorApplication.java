6d42f6a298565b98680521fab3df377e3a36d0ed
GiovanaBorges <gigi12345as17@gmail.com>
2022-07-11 22:58:43 -0300

    inital commit

---
+++ backend/src/main/java/com/devSuperior/devSuperior/DevSuperiorApplication.java
@@ -0,0 +1,13 @@
+package com.devSuperior.devSuperior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevSuperiorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevSuperiorApplication.class, args);
+	}
+
+}
